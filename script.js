c07a6a926eac57d304dc8edd42f92a71b28ecf56
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-09-10 17:04:06 -0400

    added navbar

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ $(document).ready(function () {
     $("#search-button").on("click", function () {
         var cityName = $("#search-city").val().trim()
         console.log(cityName)
-
-
         searchWeather(cityName)
 
 
@@ -16,20 +14,20 @@ $(document).ready(function () {
     function searchWeather(city) {
         $.ajax({
             type: "get",
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" +city + "&appid=1edce7f1650d0a15296eb7609bbca7a6",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1edce7f1650d0a15296eb7609bbca7a6",
             dataType: "json",
-        }).then(function(response){
-         console.log(response)
-        var card =$("<div>").addClass("card");
-        var cardTitle =$("<h1>").addClass("card-title").text(response.name)
+        }).then(function (response) {
+            console.log(response)
+            var card = $("<div>").addClass("card");
+            var cardTitle = $("<h1>").addClass("card-title").text(response.name)
 
-        // Temp, Humidity, Wind Speed, and UV Index(need to add)
-        let temp = Math.round(((response.main.temp - 273.15) * 9/5 + 32))
-         console.log("The temperature in " + city + " is: " + temp);
-         $("#todayTemp").text("Temperature: " + temp + String.fromCharCode(176)+"F");
-         $("#todayHumidity").text("Humidity: " + response.main.humidity);
-         $("#todayWindSpeed").text("Wind Speed: " + response.wind.speed);
-        $("#today").append(card.append(cardTitle))
+            // Temp, Humidity, Wind Speed, and UV Index(need to add)
+            let temp = Math.round(((response.main.temp - 273.15) * 9 / 5 + 32))
+            console.log("The temperature in " + city + " is: " + temp);
+            $("#todayTemp").text("Temperature: " + temp + String.fromCharCode(176) + "F");
+            $("#todayHumidity").text("Humidity: " + response.main.humidity);
+            $("#todayWindSpeed").text("Wind Speed: " + response.wind.speed);
+            $("#today").append(card.append(cardTitle))
         })
 
 
