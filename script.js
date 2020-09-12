9b5127eeb8eb0b1b90b4d9fe24429b95d9b586b8
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-09-11 22:57:47 -0400

    UV Index beginnng

--- script.js
+++ script.js
@@ -26,20 +26,24 @@ $(document).ready(function () {
             console.log("The temperature in " + city + " is: " + temp);
             $("#cityName").text(response.name);
             $("#todayTemp").text("Temperature: " + temp + String.fromCharCode(176) + "F");
-            $("#todayHumidity").text("Humidity: " + response.main.humidity);
+            $("#todayHumidity").text("Humidity: " + response.main.humidity + "%");
             console.log(response.main.humidity);
-            $("#todayWindSpeed").text("Wind Speed: " + response.wind.speed);
+            $("#todayWindSpeed").text("Wind Speed: " + response.wind.speed + "MPH");
             console.log(response.wind.speed);
-            $("#today").append(card.append(cardTitle))
+            $("#today").append(card.append(cardTitle));
             //UV Index
-
+            let lat = response.coord.lat;
+            let lon = response.coord.lon;
+            let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=1edce7f1650d0a15296eb7609bbca7a6 ";
+        
             // Add 5 day forecast 
         })
+        
+  
 
 
 
-
-    }
+}
 
 
 
