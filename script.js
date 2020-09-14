57922b41355eeaa796d55e310227f73652861145
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-09-13 22:40:20 -0400

    clear button

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-
+    // Search History Function 
     $("#search-button").on("click", function () {
         var cityName = $("#search-city").val().trim()
         console.log(cityName)
         searchWeather(cityName)
-     //Clear Search button 
-
-
-
     })
-    // Add Clear History Button 
+    // Clear History Function 
+    $("#clear-button").on("click", function () {
+        $(".card").remove();
+    })
+
+
+
+
+
 
     function searchWeather(city) {
         $.ajax({
@@ -20,11 +24,11 @@ $(document).ready(function () {
             console.log(response)
             var card = $("<div>").addClass("card");
             var cardTitle = $("<h1>").addClass("card-title").text(response.name)
-           
+
             // Temp, Humidity, Wind Speed, and UV Index(need to add)
             let temp = Math.round(((response.main.temp - 273.15) * 9 / 5 + 32))
             console.log("The temperature in " + city + " is: " + temp);
-            $("#cityName").text(response.name);
+            $("#cityName").text(response.name).val = today;
             $("#todayTemp").text("Temperature: " + temp + String.fromCharCode(176) + "F");
             $("#todayHumidity").text("Humidity: " + response.main.humidity + "%");
             console.log(response.main.humidity);
@@ -32,18 +36,15 @@ $(document).ready(function () {
             console.log(response.wind.speed);
             $("#today").append(card.append(cardTitle));
             //UV Index (Update as time )
-            let lat = response.coord.lat;
-            let lon = response.coord.lon;
-            let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=1edce7f1650d0a15296eb7609bbca7a6 ";
-        
+
             // Add 5 day forecast 
         })
-        
-  
 
 
 
-}
+
+
+    }
 
 
 
