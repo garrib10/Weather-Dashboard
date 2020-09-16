cc087e12e2f2022c1626ed4cbcb10bf7ace27937
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-09-16 01:38:11 -0400

    added comments

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     $("#search-button").on("click", function () {
         var cityName = $("#search-city").val().trim()
         console.log(cityName)
+        
+
         searchWeather(cityName)
     })
     // Clear History Function 
@@ -15,6 +17,8 @@ $(document).ready(function () {
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1edce7f1650d0a15296eb7609bbca7a6&units=imperial",
             dataType: "json",
         }).then(function (response) {
+          // Fix Moment for Card (#today puts it back in column)
+            var currentDate = moment().format('L')
             var temp = Math.round(response.main.temp);
             console.log(response)
             var card = $("<div>").addClass("card");
@@ -26,30 +30,32 @@ $(document).ready(function () {
             var imageIcon = $("<img>").addClass("card-image").attr({
                 "src": `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`,
                 "height": "100px", "width": "100px"
-            //UV Index// 
+
             })
             
-
             cardTitle.append(imageIcon);
             cardBody.append(cardTitle, tempDisplay, humidityDisplay, windspeedDisplay);
             card.append(cardBody);
+            //Fix this (change the div class to get it to work )
             $("#today").append(card);
 
-            getForecast(response.coord.lat,response.coord.lon)
-        });
+            getForecast(response.coord.lat, response.coord.lon)
+            
+
+        }); 
 
         //UV Index (Update as time )
         // 5-Day Forecast 
-        function getForecast (lat,lon){
-        $.ajax ({
-          type: "get",
-          dataType:"JSON",
-          url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=1edce7f1650d0a15296eb7609bbca7a6&units=imperial`,
+        function getForecast(lat, lon) {
+            $.ajax({
+                type: "get",
+                dataType: "JSON",
+                url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=1edce7f1650d0a15296eb7609bbca7a6&units=imperial`,
 
-        }).then(function(weatherData){
+            }).then(function (weatherData) {
 
 
-                for(var i =1; i < 6; i++){
+                for (var i = 1; i < 6; i++) {
                     console.log(weatherData.daily[i])
                     var day = weatherData.daily[i]
                     // Add Moment js to this using (DT parameters) 
@@ -60,11 +66,11 @@ $(document).ready(function () {
 
 
                 }
-            
 
-        })
 
- 
+            })
+
+
 
 
 
@@ -81,8 +87,8 @@ $(document).ready(function () {
 
 
 
-}) 
-    
+})
+
 
 
 
